59e7655c02651d93246a30a16dbfcd4c728dea6e
Porlar <wkkn2580@gmail.com>
2022-07-26 11:44:32 +0630

    [add] tag and category

--- pages/category.tsx
+++ pages/category.tsx
@@ -1,11 +1,21 @@
-import type { NextPage } from 'next'
+import { useCategory } from "hooks/swr";
+import Category from "models/Category";
+import type { NextPage } from "next";
 
-const Category: NextPage = () => {
-    return (
-        <div>
-            Category
-        </div>
-    )
-}
+const CategoryPage: NextPage = (props) => {
+  const { data: categories } = useCategory();
+  return (
+    <div className="px-7">
+      <h1 className="font-bold text-xl tracking-wider">Category Listing</h1>
+      <div className="flex flex-col gap-3 mt-5">
+        {categories?.map((category: any, index) => (
+          <p className="font-bold" key={index}>
+            {index + 1}. {category?.name}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Category;
+export default CategoryPage;
--- pages/tag.tsx
+++ pages/tag.tsx
@@ -1,11 +1,20 @@
-import type { NextPage } from 'next'
+import { useTag } from "hooks/swr";
+import type { NextPage } from "next";
 
-const Tag: NextPage = () => {
-    return (
-        <div>
-            Tags
-        </div>
-    )
-}
+const Tag: NextPage = (props) => {
+  const { data: tags } = useTag();
+  return (
+    <div className="px-7">
+      <h1 className="font-bold text-xl tracking-wider">Tag Listing</h1>
+      <div className="flex flex-col gap-3 mt-5">
+        {tags?.map((category: any, index: number) => (
+          <p className="font-bold" key={index}>
+            {index + 1}. {category?.name}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Tag;
